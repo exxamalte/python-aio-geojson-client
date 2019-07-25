31d21eb1a1d091d706abd17414921dda641e0194
Malte Franken <exxamalte@users.noreply.github.com>
2019-07-26 09:39:05 +1000

    changed callbacks to async

--- aio_geojson_client/feed_manager.py
+++ aio_geojson_client/feed_manager.py
@@ -15,16 +15,16 @@ _LOGGER = logging.getLogger(__name__)
 class FeedManagerBase:
     """Generic Feed manager."""
 
-    def __init__(self, feed, generate_callback, update_callback,
-                 remove_callback):
+    def __init__(self, feed, generate_async_callback, update_async_callback,
+                 remove_async_callback):
         """Initialise feed manager."""
         self._feed = feed
         self.feed_entries = {}
         self._managed_external_ids = set()
         self._last_update = None
-        self._generate_callback = generate_callback
-        self._update_callback = update_callback
-        self._remove_callback = remove_callback
+        self._generate_async_callback = generate_async_callback
+        self._update_async_callback = update_async_callback
+        self._remove_async_callback = remove_async_callback
 
     def __repr__(self):
         """Return string representation of this feed."""
@@ -45,13 +45,13 @@ class FeedManagerBase:
             feed_external_ids = set(self.feed_entries)
             remove_external_ids = self._managed_external_ids.difference(
                 feed_external_ids)
-            self._remove_entities(remove_external_ids)
+            await self._remove_entities(remove_external_ids)
             update_external_ids = self._managed_external_ids.intersection(
                 feed_external_ids)
-            self._update_entities(update_external_ids)
+            await self._update_entities(update_external_ids)
             create_external_ids = feed_external_ids.difference(
                 self._managed_external_ids)
-            self._generate_new_entities(create_external_ids)
+            await self._generate_new_entities(create_external_ids)
         elif status == UPDATE_OK_NO_DATA:
             _LOGGER.debug(
                 "Update successful, but no data received from %s", self._feed)
@@ -59,30 +59,30 @@ class FeedManagerBase:
             _LOGGER.warning(
                 "Update not successful, no data received from %s", self._feed)
             # Remove all entities.
-            self._remove_entities(self._managed_external_ids.copy())
+            await self._remove_entities(self._managed_external_ids.copy())
             # Remove all feed entries and managed external ids.
             self.feed_entries.clear()
             self._managed_external_ids.clear()
 
-    def _generate_new_entities(self, external_ids):
+    async def _generate_new_entities(self, external_ids):
         """Generate new entities for events."""
         for external_id in external_ids:
-            self._generate_callback(external_id)
+            await self._generate_async_callback(external_id)
             _LOGGER.debug("New entity added %s", external_id)
             self._managed_external_ids.add(external_id)
 
-    def _update_entities(self, external_ids):
+    async def _update_entities(self, external_ids):
         """Update entities."""
         for external_id in external_ids:
             _LOGGER.debug("Existing entity found %s", external_id)
-            self._update_callback(external_id)
+            await self._update_async_callback(external_id)
 
-    def _remove_entities(self, external_ids):
+    async def _remove_entities(self, external_ids):
         """Remove entities."""
         for external_id in external_ids:
             _LOGGER.debug("Entity not current anymore %s", external_id)
             self._managed_external_ids.remove(external_id)
-            self._remove_callback(external_id)
+            await self._remove_async_callback(external_id)
 
     @property
     def last_timestamp(self) -> Optional[datetime]:
